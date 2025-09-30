b3dd65454dc1ecd0f152d8ccca0c9365ed8c53a4
ganeshjadhav9361 <ganeshsjadhav3112@gmail.com>
2025-09-30 20:40:40 +0530

    SpringCoreSecondApplication projects

---
+++ SpringCoreSecondApplication/src/main/java/org/program/ClientApplication.java
@@ -0,0 +1,16 @@
+package org.program;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context= new ClassPathXmlApplicationContext("test.xml");
+		Company c= (Company)context.getBean("c");
+		
+		c.show();
+	}
+
+}
---
+++ SpringCoreSecondApplication/src/main/java/org/program/Company.java
@@ -0,0 +1,16 @@
+package org.program;
+
+public class Company {
+	private Employee employee;
+	public void setEmployee(Employee employee) {
+		this.employee=employee;
+	}
+	
+	public Employee getEmployee() {
+		return employee;
+	}
+	
+	public void show() {
+		System.out.println(employee.getId()+"\t"+employee.getName()+"\t"+employee.getSal());
+	}
+}
